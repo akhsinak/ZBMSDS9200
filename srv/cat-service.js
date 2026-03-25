690041d852457c9598f3e44e1cbb5cd98cc986a9
Kanishka Sai Tulasi Ponnada <kanishka.sai.t.p@inx.co.jp>
2026-03-25 05:54:23 +0000

    User specific Emails

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -1,5 +1,15 @@
 const cds = require('@sap/cds')
 
-module.exports = class EmailService extends cds.ApplicationService { init() {
-  return super.init()
-}}
+module.exports = class EmailService extends cds.ApplicationService {
+  init() {
+    this.on('READ', 'Emails', async (req, next) => {
+      const userId = req.user && req.user.id
+      if (userId && req.query && req.query.SELECT) {
+        req.query.where({ createdBy: userId })
+      }
+      return next()
+    })
+
+    return super.init()
+  }
+}
